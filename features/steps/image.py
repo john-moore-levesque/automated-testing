effe9664ba1eeaa23f8bb207306ba6a83edc733c
John Moore-Levesque <john@moorelevesque.com>
2021-03-18 09:34:47 -0400

    Converting image to jpg, since PNG causes it to fail silently

--- features/steps/image.py
+++ features/steps/image.py
@@ -1,14 +1,22 @@
 from behave import *
 from PIL import Image, ImageChops
 from datetime import *
-
+from os import path
 
 @then(u'the screenshot matches the golden image "{filename}"')
-def compare_screeshot(context, filename):
+def compare_screenshot(context, filename):
+    def _convert(screenshotPath):
+        screenshot = Image.open("%s" %(screenshotPath))
+        screenshot = screenshot.convert("RGB")
+        f, e = path.splitext(screenshotPath)
+        outfile = f + ".jpg"
+        screenshot.save(outfile)
+        return outfile
+
     screenshotPath = "%s/%s.png" %(context.browser.scratch, datetime.strftime(datetime.now(), "%Y%m%d%H%M%S"))
     screenshot = context.browser.browser.save_screenshot("%s" %(screenshotPath))
     golden = Image.open("images/%s" %(filename))
-    screenshot = Image.open("%s" %(screenshotPath))
+    screenshot = Image.open(_convert(screenshotPath))
     diff = ImageChops.difference(golden, screenshot)
     if diff.getbbox():
         print("%s differs from the golden image!" %(screenshotPath))
